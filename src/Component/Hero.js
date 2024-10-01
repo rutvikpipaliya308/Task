55909ad23ad871ad8bd232b9332256d72ace1c13
Rutvik <rmpipaliya308@gmail.com>
2024-10-01 23:38:07 +0530

    First section completed

---
+++ src/Component/Hero.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from './Navbar';
+
+export const Hero = () => {
+    return (
+        <section className="relative w-full h-[968px] bg-cover bg-center top-0 bg-slate-500"
+            style={{ backgroundImage: 'url("/Images/Home.png")' }}>
+
+            <Navbar />
+
+            {/* Text Content */}
+            <div className='flex flex-col justify-center items-center h-full'>
+                <div className="flex flex-col justify-between w-[1128px] h-[300px] text-center text-white">
+                    <div className='h-[220px] flex flex-col gap-[24px]'>
+                        <h1 className="font-semibold text-[72px] text-customWhite leading-[83px]">
+                            Vivamus pretium elit,<br /> venenatis quam scelerisque non.
+                        </h1>
+                        <p className="text-[20px] leading-[28px] font-semibold text-customWhite">
+                            Fusce egestas fermentum elit ac sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </p>
+                    </div>
+
+                    <button className="bg-customPurple text-customWhite w-[166px] h-[48px] mb-0 p-[12px] pl-[32px] pr-[24px] text-[16px] rounded-full flex m-auto items-center">
+                        <p>Let's Start</p>
+                        <span className="material-icons"><img src='/Images/Icon.png'></img></span>
+                    </button>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="w-full flex justify-between items-center pt-[24px] pl-[108px] pr-[108px] pb-[24px] h-[96px] bg-transparent">
+            <div className="flex items-center">
+                <img src='/Images/Logo.png' className="h-[32px] w-[32px]"></img>
+                <p className="h-[28px] w-[43px] font-normal text-base text-white">Logo</p>
+            </div>
+            <div className="flex items-center gap-[48px] h-[48px]">
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">Askitect AI</a>
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">Solutions</a>
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">Pricing</a>
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">About Us</a>
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">Contact Us</a>
+                <a href="#" className="text-customWhite h-[28px] text-[18px]">中国人</a>
+                <div className=" flex w-[257px] gap-[24px] justify-between">
+                    <button className="text-white bg-customPurple rounded-[24px] p-[12px_32px]" >Log In</button>
+                    <button className="text-white rounded-[24px] p-[12px_32px] border border-white">Sign Up</button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
